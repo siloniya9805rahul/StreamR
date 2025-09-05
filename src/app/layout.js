5ded52669d06cc953ade70e15f1f60f632ecfa2e
Rahul Siloniya <rahulsiloniya9805@gmail.com>
2025-09-05 14:30:07 +0530

    ninth commit StreamR

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,11 @@
 
 import { ToastContainer } from "react-toastify";
 import "./globals.css";
-import RouteLoader from "@/components/RouteLoader";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <RouteLoader />
 
         {children}
 
